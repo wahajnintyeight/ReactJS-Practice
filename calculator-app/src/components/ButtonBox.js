77836b786395798379275c36978db4a1a5a353e3
wahajnintyeight <wahajdorift@yahoo.com>
2021-12-26 13:52:09 +0500

    added components

---
+++ calculator-app/src/components/ButtonBox.js
@@ -0,0 +1,38 @@
+import Wrapper from "./Wrapper";
+import Screen from "./Screen";
+import ButtonBox from "./ButtonBox";
+import Button from "./Button";
+
+const btnValues = [
+  ["C", "+-", "%", "/"],
+  [7, 8, 9, "X"],
+  [4, 5, 6, "-"],
+  [1, 2, 3, "+"],
+  [0, ".", "="],
+];
+
+const App = () => {
+  return (
+    <Wrapper>
+      <Screen value="0" />
+      <ButtonBox>
+        {
+          btnValues.flat().map((btn, i) => {
+            return (
+              <Button
+                key={i}
+                className={btn === "=" ? "equals" : ""}
+                value={btn}
+                onClick={() => {
+                  console.log(`${btn} clicked!`);
+                }}
+              />
+            );
+          })
+        }
+      </ButtonBox>
+    </Wrapper>
+  );
+};
+
+export default ButtonBox;
